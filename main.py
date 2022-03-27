abae2c2a6bbb9ee880b8620a60e4c01b2a842c25
__youling__ <3074377329@qq.com>
2022-03-27 12:44:24 +0800

    wordle

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+
 from rich import print
+from word import randWord, findWord, compare
 
-print("begin")
 
-for i in range(10):
-    print(i,"Hello World!" * i)
-
-print("Finish")
+anwser = randWord()
+print(anwser)
+# 记次用的
+Count = 0
+# 输入六次
+for i in range(6):
+    userInput = input("请输入单词:")
+    # 检测长度
+    if len(userInput) != 5 or findWord(userInput) is False:
+        print("请您输入5个字母的英语单词")
+    else:
+        print(compare(anwser, userInput))
+print("垃圾，这都猜不出来，这说明什么，这说明你就是个大混子")
